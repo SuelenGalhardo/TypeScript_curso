7ec034a98956e7d31f02ac71100daca159babf7b
SuelenGalhardo <suelenranucci@gmail.com>
2026-02-03 20:49:46 +0100

    numbers

---
+++ tipos/numbers.ts
@@ -0,0 +1,23 @@
+(() =>  {
+  
+
+    let pokemon: number = 123;
+
+   // console.log(pokemon);
+
+    const dagonBall: number = 500;
+
+     if( pokemon < dagonBall){
+     console.log('es mejor');
+     } else {
+
+        console.log('no es mejor');
+     }
+
+
+     //NaN
+    // pokemon = Number('1234A');
+     pokemon = Number('1234');  
+     console.log({ pokemon });
+
+})()
